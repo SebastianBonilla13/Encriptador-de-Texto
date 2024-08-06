c1d4a10aa8fa0cd0a09bed6904a3523a59c1b084
SebastianBonilla13 <107320080+SebastianBonilla13@users.noreply.github.com>
2024-08-06 16:04:00 -0500

    state no encontrado

--- src/AppEncriptador.jsx
+++ src/AppEncriptador.jsx
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 import LogoAlura from "./assets/Logo.png";
 
 import { IoAlertCircle } from "react-icons/io5";
+import { MdSearchOff } from "react-icons/md";
 
 
 function AppEncriptador() {
@@ -39,7 +40,7 @@ function AppEncriptador() {
 
   const handleValidate = () => {
     //identifica input, identifica tecla, activa evento
-    document.getElementById('varTexto').addEventListener('keypress', function (event) { 
+    document.getElementById('varTexto').addEventListener('keypress', function (event) {
       const regex = /[a-zñ\s]/; // expresión regular, todas las letras minúsculas y "espacio"
       const key = String.fromCharCode(event.keyCode); // captura caracter presionado
       console.log('validando letra = ', key);
@@ -72,13 +73,13 @@ function AppEncriptador() {
 
       <body>
 
-        <div class=" p-4 bg-neutral-200 h-screen">
+        <div class=" p-4 bg-neutral-900 h-screen">
 
           {/*  */}
-          <div class=" grid grid-cols-3 gap-4 h-full w-full    g-neutral-700"> {/* flex justify-between */}
+          <div class="grid  grid-rows-6 grid-cols-6    gap-4  h-full w-full    g-neutral-700"> {/*grid-col-3   flex justify-between */}
 
             {/* Screen Izq. */}
-            <div class="col-span-2 bg-neutral-500"> {/* w-2/3  */}
+            <div class="row-span-5 col-span-6   lg:row-span-6 lg:col-span-4  bg-neutral-500"> {/*col-span-2  w-2/3  */}
 
               {/* LOGO + TEXTO container */}
               <div class="grid grid-cols-8 gap-3 bg-red-200 h-full "> {/* <!-- grid auto-rows-auto --> */} {/* flex justify-center */}
@@ -91,7 +92,7 @@ function AppEncriptador() {
                 </div>
 
                 {/* Der. container */}
-                <div class=" col-span-7 grid grid-rows-8 gap-4 pb-4 bg-slate-800">
+                <div class=" col-span-7 grid grid-rows-9 gap-4 pb-4 bg-slate-800">
 
                   {/* TITULO container */}
                   <div class="row-span-1 text-2xl md:text-3xl bg-red-400">
@@ -101,7 +102,7 @@ function AppEncriptador() {
                   </div>
 
                   {/* TEXTO container */}
-                  <div class=" row-span-5 bg-purple-400">
+                  <div class=" row-span-6 bg-purple-400">
                     <form class="w-full  h-full bg-red-500">
                       <textarea class="h-full w-full outline-none resize-none bg-transparent text-sky-900" id="varTexto" placeholder="Ingrese texto aquí..." onChange={handleValidate}></textarea>
                     </form>
@@ -121,7 +122,7 @@ function AppEncriptador() {
                     </div>
 
                     {/* BOTONES container */}
-                    <div class="grid grid-cols-2 gap-4 px-1 g-red-400 "> {/* flex flex-row justify-center gap-4 */}
+                    <div class="grid grid-cols-2 gap-4 px-1 bg-red-400 "> {/* flex flex-row justify-center gap-4 */}
 
                       <button class=" text-white bg-sky-900 w-full h-14 rounded-2xl ring-1 ring-sky-900 "
                         onClick={handleEncriptar}>
@@ -143,31 +144,50 @@ function AppEncriptador() {
             </div>
 
             {/* Screen Der. */}
-            <div class="col-span-1 rounded-2xl bg-white "> {/* w-1/3 */}
-              
+            <div class="row-span-1 col-span-6    lg:col-span-2 lg:row-span-6    rounded-2xl bg-white "> {/*col-span-1  w-1/3 */}
+
+
               <div class="grid grid-rows-8 h-full py-4 px-5 gap-4">
 
-                <div class=" row-span-7 bg-lime-300">
+                <div class=" row-span-7 g-lime-300">
                   <form class="w-full h-full">
-                    <textarea class="h-full w-full outline-none resize-none bg-transparent text-zinc-600" id="TextareaRead" value={texto} /* placeholder="Ingrese texto aquí..." onChange={handleValidate} */></textarea>
+                    {/* <textarea class="h-full w-full outline-none resize-none bg-transparent text-zinc-600" id="TextareaRead" value={texto}></textarea> */}
+
+                    {(texto) &&
+                      <div class="h-full w-full ">
+                        <textarea class="h-full w-full outline-none resize-none bg-transparent text-zinc-600" id="TextareaRead" value={texto}></textarea>
+                      </div>
+                    }
+
+                    {(!texto) &&
+                      <div class=" h-full flex justify-center items-center g-red-300  "> {/* grid grid-rows-3 */}
+                        <div>
+                          <div class="flex justify-center">
+                            <MdSearchOff class=" w-12 h-12  lg:w-20 lg:h-20" color="#858585" />
+                          </div>
+                          
+                          <p class=" text-xl lg:text-2xl font-bold text-zinc-500 text-center">
+                            Ningún mensaje fue encontrado
+                          </p>
+                          <p class=" text-sm lg:text-base text-zinc-500 text-center">
+                            Ingresa el texto que desees encriptar o desencriptar.
+                          </p>
+                        </div>
+                      </div>
+                    }
+
                   </form>
                 </div>
 
-                {/* <div class="row-span-1 flex justify-center items-center  px-6 g-red-700" >
-                  <button class=" text-sky-900 bg-white w-full h-14 rounded-2xl ring-1 ring-sky-900 "
-                    onClick={handleCopiar}>
-                    Copiar
-                  </button>
-                </div> */}
-
-                <div class="row-span-1">
+                {(texto) &&
+                 <div class="row-span-1">
                   <button class=" text-sky-900 bg-white w-full h-14 rounded-2xl ring-1 ring-sky-900 "
                     onClick={handleCopiar}>
                     Copiar
                   </button>
                 </div>
+               }
                 
-
 
               </div>
 
@@ -187,5 +207,6 @@ function AppEncriptador() {
     </div>
   );
 }
+
 
 export default AppEncriptador;
